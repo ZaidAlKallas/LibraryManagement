579b38b1e18c901ea8edad30d4861b8e893b2d6c
ZaidAlKallas <eng.zaid.kallas@gmail.com>
2025-08-24 13:03:51 +0300

    Add ServiceCollectionExtensions for DbContext configuration

---
+++ src/Library.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Library.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Library.Infrastructure.Extensions;
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddLibraryDbContext(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var dbProvider = configuration["DatabaseProvider"] ?? "SqlServer";
+
+        services.AddDbContext<LibraryDbContext>(options =>
+        {
+            if (dbProvider == "SqlServer")
+                options.UseSqlServer(configuration.GetConnectionString("SqlServer"));
+            else if (dbProvider == "Sqlite")
+                options.UseSqlite(configuration.GetConnectionString("Sqlite"));
+        });
+
+        return services;
+    }
+}
