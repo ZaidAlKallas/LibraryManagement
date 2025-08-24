ad83b4f0551f3bdaac88bd374a8212762956081a
ZaidAlKallas <eng.zaid.kallas@gmail.com>
2025-08-24 13:04:26 +0300

    Add ApplicationBuilderExtensions for auto migrations

---
+++ src/Library.Infrastructure/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Library.Infrastructure.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Library.Infrastructure.Extensions;
+
+public static class ApplicationBuilderExtensions
+{
+    public static IApplicationBuilder ApplyMigrations(this IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        var db = scope.ServiceProvider.GetRequiredService<LibraryDbContext>();
+        db.Database.Migrate();
+        return app;
+    }
+}
