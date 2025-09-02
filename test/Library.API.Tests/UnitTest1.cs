95e27ad7d138066b9a2b6edb988aeb81c8b073e6
ZaidAlKallas <eng.zaid.kallas@gmail.com>
2025-09-03 00:04:09 +0300

    test: setup base testing structure for Application and API layers

---
+++ test/Library.API.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System.Net;
+
+namespace Library.API.Tests;
+
+public class HealthCheckTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client;
+
+    public HealthCheckTests(WebApplicationFactory<Program> factory) => _client = factory.CreateClient();
+
+    [Fact]
+    public async Task RootEndpoint_ShouldReturnNotFound()
+    {
+        var response = await _client.GetAsync("/");
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+}
---
+++ test/Library.Application.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+﻿using FluentAssertions;
+
+namespace Library.Application.Tests;
+
+public class DummyTests
+{
+    [Fact]
+    public void Sanity_Check() => true.Should().BeTrue();
+}
