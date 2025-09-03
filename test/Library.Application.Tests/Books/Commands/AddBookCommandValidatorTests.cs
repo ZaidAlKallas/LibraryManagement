6abfec7588c47c30087c99582290d81a0d164758
ZaidAlKallas <eng.zaid.kallas@gmail.com>
2025-09-04 00:04:09 +0300

    test: add unit tests for AddBookCommandValidator

---
+++ test/Library.Application.Tests/Books/Commands/AddBookCommandValidatorTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentValidation.TestHelper;
+using Library.Application.Books.Commands.AddBook;
+
+namespace Library.Application.Tests.Books.Commands;
+
+public class AddBookCommandValidatorTests
+{
+    private readonly AddBookCommandValidator _validator = new();
+
+    [Fact]
+    public void Should_Have_Error_When_Title_Is_Empty()
+    {
+        var command = new AddBookCommand("", "1234567890", [1], "TestPublisher");
+
+        var result = _validator.TestValidate(command);
+
+        result.ShouldHaveValidationErrorFor(c => c.Title);
+    }
+
+    [Fact]
+    public void Should_Have_Error_When_ISBN_Is_Empty()
+    {
+        var command = new AddBookCommand("Test Title", "", [1], "TestPublisher");
+
+        var result = _validator.TestValidate(command);
+
+        result.ShouldHaveValidationErrorFor(c => c.ISBN);
+    }
+
+    [Fact]
+    public void Should_Not_Have_Error_When_All_Fields_Are_Valid()
+    {
+        var command = new AddBookCommand("Clean Code", "9780132350884", [1], "Prentice Hall");
+
+        var result = _validator.TestValidate(command);
+
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+}
--- test/Library.Application.Tests/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-﻿using FluentAssertions;
-
-namespace Library.Application.Tests;
-
-public class DummyTests
-{
-    [Fact]
-    public void Sanity_Check() => true.Should().BeTrue();
-}
